1797b423f4e9d8bb4fe98d5fa4f8f981190b53c3
Muzaky <afif050503@gmail.com>
2024-12-04 18:48:54 +0700

    port changes

--- apiflask.py
+++ apiflask.py
@@ -158,4 +158,6 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0',port=port, debug=True)
+
